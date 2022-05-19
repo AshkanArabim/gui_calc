fbd918bdd623d34927a6df09684f685db9835c4e
Ashkan <ashkan.arabim@gmail.com>
2022-05-19 17:19:45 -0600

    add button functions object

--- script.js
+++ script.js
@@ -22,10 +22,71 @@ function chsign(a) {
   return -a;
 }
 
-// function operator() {
+const btnFuncs = {
+  clear() {
+    console.log("clear");
+  },
+  back() {
+    console.log("back");
+  },
+  chsign() {
+    console.log("change sign");
+  },
+  rad() {
+    console.log("sqaure root");
+  },
+  n7() {
+    console.log("number 7");
+  },
+  n8() {
+    console.log("number 8");
+  },
+  n9() {
+    console.log("number 9");
+  },
+  divide() {
+    console.log("divide");
+  },
+  n4() {
+    console.log("number 9");
+  },
+  n5() {
+    console.log("number 5");
+  },
+  n6() {
+    console.log("number 9");
+  },
+  multiply() {
+    console.log("multiply");
+  },
+  n1() {
+    console.log("number 9");
+  },
+  n2() {
+    console.log("number 2");
+  },
+  n3() {
+    console.log("number 3");
+  },
+  subtract() {
+    console.log("subtract");
+  },
+  n0() {
+    console.log("number 0");
+  },
+  decim() {
+    console.log("decimal point");
+  },
+  operate() {
+    console.log("operator");
+  },
+  add() {
+    console.log("add");
+  },
+};
 
-// }
+const btns = new Array(...document.querySelectorAll(".btns button"));
 
-// function keylogger() {
-
-// }
+for (let x in btns) {
+  btns[x].addEventListener("click", btnFuncs[x]);
+}
