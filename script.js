03aa3d344edeb6d4856775453fd1bac6bf1727dc
Ashkan <ashkan.arabim@gmail.com>
2022-05-19 00:00:03 -0600

    add operator funcs

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function sqrt(a) {
+  return a ** 0.5;
+}
+
+function chsign(a) {
+  return -a;
+}
